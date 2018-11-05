3e14faaa9dc1a10ac837eb067e8a53a91d277932
mts299 <mts299888@gmail.com>
2018-11-05 16:13:33 -0600

    Pydarn logging comments
    Added comments and licensing for pydarns logging.
    Included the dmap testing, it fail currently but show the test case
    of how pydarn produces log files for info and errors.
    
    
    Former-commit-id: 32637db3068f26a72ab017be251746dcb0f95930

--- setup.py
+++ setup.py
@@ -16,7 +16,6 @@ import sys
 from subprocess import check_call
 from setuptools.command.install import install, orig
 
-
 # This class and function overrides the install python
 # setup method to add an extra git command in to install
 # the submodule
