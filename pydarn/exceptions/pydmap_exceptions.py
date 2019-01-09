8f82ab29638a096d6618fafbe2723157c043cdd6
Marina Schmidt <marina@auroral.usask.ca>
2019-01-09 09:03:48 -0600

    Updating exceptions docstring
    Added a new exception for negative byte cases.
    Updated the doctstring of pydmap_expections to
    numpy style and added more information into the doctstring.
    Also, PEP 8 style changes and type hinting.
    
    
    Former-commit-id: 8707cc182a293389026ffa0a7fcdecdd3b45218d

--- pydarn/exceptions/pydmap_exceptions.py
+++ pydarn/exceptions/pydmap_exceptions.py
@@ -6,30 +6,48 @@
 import logging
 pydarn_logger = logging.getLogger('pydarn')
 
+
 class CursorError(Exception):
     """
     Raise if the cursor is not correctly set
+
+    Parameter
+    ---------
+    cursor : int
+        Current position in the data buffer
+    expected_value : int
+        What you expected the value of the cursor to be.
+        Default 0.
+    message : str
+        Another type of message that might give more information
+        on the cursor error.
+        Default empty.
     """
-    def __init__(self,cursor, expected_value=0, mesage=''):
+    def __init__(self, cursor: int, expected_value=0, message=''):
         self.cursor
         if message == '':
             self.message = "Error: Cursor is at {cursor} and"\
                     "it needs to be {expected}".format(cursor=cursor,
                                                        expected=expected_value)
         else:
-            self.message=message
+            self.message = message
         super().__init__(self.message)
         pydarn_logger.error(self.message)
 
 
 class EmptyFileError(Exception):
     """
-    Raised if a file is empty
+    Raised if a file is empty.
+
+    Parameter
+    --------
+    filename : str
+        The name of the file that is empty.
     """
-    def __init__(self,filename):
+    def __init__(self, filename: str):
         self.filename = filename
         self.message = "Error: {} is empty or"\
-                " please check this is the correct file".format(filename)
+            " please check this is the correct file".format(filename)
         super().__init__(self.message)
         pydarn_logger.error(self.message)
 
@@ -37,68 +55,114 @@ class EmptyFileError(Exception):
 class DmapDataTypeError(Exception):
     """
     Raised if the data type is not correct.
+
+    Parameter
+    ---------
+    filename : str
+        name of the file that the DMAP data is coming from.
+    data_name : str
+        parameter field name in the DMAP record
+    data_type : str
+        data type of the field name.
+    cursor : int
+        current position in the data buffer
     """
-    def __init__(self, filename, data_name, data_type, cursor):
+    def __init__(self, filename: str, data_name: str,
+                 data_type: str, cursor: int):
         self.filename = filename
         self.message = "Error: Dmap data type {data_type} for {name}"\
-                " at cursor = {cursor} does not exist in dmap data types."\
-                "filename: {filename}".format(name=data_name,
-                                              data_type=data_type,
-                                              filename=filename,
-                                              cursor=cursor)
+            " at cursor = {cursor} does not exist in dmap data types."\
+            "filename: {filename}".format(name=data_name,
+                                          data_type=data_type,
+                                          filename=filename,
+                                          cursor=cursor)
+        super().__init__(self.message)
+        pydarn_logger.error(self.message)
+
+
+class NegativeByteError(Exception):
+    """
+    Raised if an element has < 0 bytes.
+
+    Parameter
+    ---------
+    filename : str
+        The file name of the DMAP data
+    element_info : str
+        String containing element name.
+    """
+    def __init__(self, filename: str, element_info: str, cursor: int):
+        self.filename = filename
+        self.message = "Error: {filename} contains an {element} < 0"\
+            " at cursor = {cursor}.".format(filename=filename,
+                                            element=element_info,
+                                            cursor=cursor)
         super().__init__(self.message)
         pydarn_logger.error(self.message)
 
 
 class ZeroByteError(Exception):
     """
-    Raised if an element has <= 0 bytes.
-        :pram filename: The file that contains the element
-        :pram element_info: String containg element info.
+    Raised if an element has == 0 bytes.
+
+    Parameter
+    ---------
+    filename : str
+        Name of the file that contains the DMAP data
+    element_info : str
+        String containing element name.
+    cursor : int
+        Current position in the dmap buffer
     """
-    def __init__(self,filename, element_info, cursor):
+    def __init__(self, filename: str, element_info: str, cursor: int):
         self.filename = filename
-        self.message = "Error: {filename} contains an {element} <= 0"\
-                " at cursor = {cursor}.".format(filename=filename,
-                                              element=element_info,
-                                              cursor=cursor)
+        self.message = "Error: {filename} contains an {element} == 0"\
+            " at cursor = {cursor}.".format(filename=filename,
+                                            element=element_info,
+                                            cursor=cursor)
         super().__init__(self.message)
         pydarn_logger.error(self.message)
 
 
 class MismatchByteError(Exception):
     """
-    Raised if an element has <= 0 bytes.
-        :pram filename: The file that contains the element
-        :pram element_info: String containg element info.
+    Raised if an element is > another element.
+
+    Parameter
+    ---------
+    filename : str
+        Name of the file that contains the DMAP data
+    element_info : str
+        String containing element info on the two elements being compared.
+    cursor : int
+        Current position in the dmap buffer.
     """
-    def __init__(self, filename, element_info, cursor):
+    def __init__(self, filename: str, element_info: str, cursor: int):
         self.filename = filename
         self.message = "Error: {filename} contains an {element}"\
-                " at cursor = {cursor}.".format(filename=filename,
-                                              element=element_info,
-                                              cursor=cursor)
+            " at cursor = {cursor}.".format(filename=filename,
+                                            element=element_info,
+                                            cursor=cursor)
         super().__init__(self.message)
         pydarn_logger.error(self.message)
 
 
+# TODO: may need to delete later if not used in DmapWrite?
 class DmapDataError(Exception):
     """Raised if there is an error in parsing of data
 
+    Parameter
+    ---------
+    filename : str
+        Name of the file that contains the DMAP data
+    message : str
+        Message containing information on the Error
     """
-    def __init__(self,filename, message):
+    def __init__(self, filename: str, message: str):
         self.filename = filename
         self.message = "Error: {filename} is data,"\
-                " is corrupt because: {msg}".format(filename=filename,
-                                                    msg=message)
+            " is corrupt because: {msg}".format(filename=filename,
+                                                msg=message)
         super().__init__(self.message)
         pydarn_logger.error("DmapDataError: {} is corrupt {}".format(filename,
                                                                      message))
-
-
-class CorruptDataError(Exception):
-    """
-    Raise when data is corrupt in the file
-    """
-    def __init__():
-        pass
