acc1859594b1a04d841b4a46de0490902ddef246
Marina Schmidt <marina@auroral.usask.ca>
2019-01-28 11:46:25 -0600

    Started testing dmap_write
    Started with terminal testing and setup.py.
    Compiles and runs like normal for read. However, unit and integration
    tests still need to be written for dmap write. Data sets file is
    for making pseudo data sets that user may input into the dmap write
    to create a file.
    
    
    Former-commit-id: a541cbfb59e560f560a90baa7bcdc52cc0ccc225

---
+++ test/pydmap_tests/data_sets.py
@@ -0,0 +1,35 @@
+"""
+Test data sets for DmapWrite
+"""
+
+fitacf_data = []
+
+rawacf_data = []
+
+iqdat_data = []
+
+grid_data = []
+
+map_data = []
+
+dmap_data = []
+
+missing_fitacf_field = []
+
+missing_rawacf_field = []
+
+missing_grid_field = []
+
+missing_map_field = []
+
+missing_iqdat_field = []
+
+extra_fitacf_field = []
+
+extra_rawacf_field = []
+
+extra_grid_field = []
+
+extra_map_field = []
+
+extra_iqdat_field = []
