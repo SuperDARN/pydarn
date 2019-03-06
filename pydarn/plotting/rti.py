a33a05777a1b6fe7498f08a8fbfacecf111f4483
Marina Schmidt <marina@auroral.usask.ca>
2019-03-06 16:00:50 -0600

    Started adding a Radar information sheet
    This Radar sheet will allow for quick access to information on SuperDARN
    Radars to be displayed on plots.

---
+++ pydarn/plotting/rti.py
@@ -0,0 +1,33 @@
+# Copyright (C) 2019 SuperDARN
+# Author: Marian Schmidt
+# This code is improvement based on rti.py in the DaVitpy library
+# https://github.com/vtsuperdarn/davitpy/blob/master/davitpy
+
+"""
+Range-time Intensity plots
+"""
+import matplotlib
+import typing
+import pydarn
+
+class RTI(matplotlib):
+    """
+    Range-time intensity plots SuperDARN data using the following fields:
+
+
+    Notes
+    -----
+    This class inherits from matplotlib to generate the figures
+
+    Methods
+    -------
+    plot
+    summaryplot
+
+    """
+    def plot(self, dmap_data: List[dict], ):
+        pass
+
+    def summaryplot(self):
+        pass
+
