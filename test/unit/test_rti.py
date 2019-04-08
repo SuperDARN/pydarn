42413186aacc287661cf00ee0a8bce595b2bb888
Marina Schmidt <marina@auroral.usask.ca>
2019-04-08 16:21:06 -0600

    X-axis date labels correctly displayed
    Got the x-axis date labels fixed with the user's ability to change the
    format. Colour bar is also added with a label but users can change if
    desired.

--- test/unit/test_rti.py
+++ test/unit/test_rti.py
@@ -1,8 +1,10 @@
 import pydarn
+import matplotlib.pyplot as plt
 
 fitacf_file = "../testfiles/20181209.C0.sas.fitacf"
 darn_read = pydarn.DarnRead(fitacf_file)
 fitacf_data = darn_read.read_fitacf()
 
-pydarn.RTI.plot_profile(fitacf_data, paramete='elevation', beamnum=7)
-
+pydarn.RTI.plot_profile(fitacf_data, parameter='elevation', beamnum=7, ground_scatter=False)
+plt.title("Elevation")
+plt.show()
