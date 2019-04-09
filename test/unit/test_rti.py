84ddf88f4157475c33fd8c49d3469310b522afae
Marina Schmidt <marina@auroral.usask.ca>
2019-04-09 16:34:15 -0600

    Added axes object and refactored code
    Added axes object, upon request of Ashton Remier when a user
    wants to use the axes object in matplotlib.
    Refactored some code and added a plotting error.

--- test/unit/test_rti.py
+++ test/unit/test_rti.py
@@ -5,6 +5,12 @@ fitacf_file = "../testfiles/20181209.C0.sas.fitacf"
 darn_read = pydarn.DarnRead(fitacf_file)
 fitacf_data = darn_read.read_fitacf()
 
-pydarn.RTI.plot_profile(fitacf_data, parameter='elevation', beamnum=7, ground_scatter=False)
+plt.subplot(2, 1, 1)
+pydarn.RTP.plot_range_time(fitacf_data, parameter='elevation', beamnum=7, ground_scatter=False)
 plt.title("Elevation")
+
+plt.subplot(2, 1, 2)
+t = range(0, 20)
+s = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+plt.plot(t,s)
 plt.show()
