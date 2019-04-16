0668b2b6777dcad0248843ce5689efc80d3f1fbf
mardet987 <marci.detwiller@usask.ca>
2019-04-16 16:35:41 -0600

    Borealis converter util; BorealisFileHandler class
    and created the BorealisConversionTypesError exception
    
    
    Former-commit-id: 0f179a93ebb5cd55ba8b50c5f7dff21eb8a49f9c

--- pydarn/exceptions/borealis_exceptions.py
+++ pydarn/exceptions/borealis_exceptions.py
@@ -11,14 +11,16 @@ class BorealisConversionTypesError(Exception):
     --------
     filename : str
         name of the file attempting to convert
+    origin_filetype: str
+        Borealis origin filetype, ex. bfiq.
     dmap_filetype : str
-        desired SuperDARN legacy dmap type 
+        desired SuperDARN legacy dmap type, ex. iqdat.
     """
     def __init__(self, filename: str, origin_filetype: str, dmap_filetype: str):
         self.filename = filename
         self.origin_filetype = origin_filetype
         self.dmap_filetype = dmap_filetype
-        self.message = "Error: {filename} cannot be converted from its origin_filetype "
-                       "{origin_filetype} to legacy dmap filetype {dmap_filetype} because the "
-                       "types are not compatible.".format(filename=self.filename,
+        self.message = "Error: {filename} cannot be converted from its origin_filetype "\
+                       "{origin_filetype} to legacy dmap filetype {dmap_filetype} because the "\
+                       "types are not compatible or currently not supported.".format(filename=self.filename,
                         origin_filetype=self.origin_filetype, dmap_filetype=self.dmap_filetype)
