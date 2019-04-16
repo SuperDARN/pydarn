50014688e7b004729d9b007cc42a22196093ae89
Marina Schmidt <marina@auroral.usask.ca>
2019-04-15 18:21:37 -0600

    Add some extra features into the plotting
    Fixed up the theme to be similar to davitpy's then
    added a couple extra features around colour mapping.
    Tested the time range ability.
    
    Currently the x-axis origin doesn't show :(, trying to
    figure that out.

--- test/unit/test_rti.py
+++ test/unit/test_rti.py
@@ -1,15 +1,14 @@
 import pydarn
 import matplotlib.pyplot as plt
+from datetime import datetime
 
 fitacf_file = "../testfiles/20181209.C0.sas.fitacf"
 darn_read = pydarn.DarnRead(fitacf_file)
 fitacf_data = darn_read.read_fitacf()
 
-print(len(fitacf_data))
-for i in range(2000, 2300):
-    del fitacf_data[i]
-print(len(fitacf_data))
 
-pydarn.RTP.plot_range_time(fitacf_data, parameter='elevation', beamnum=7, ground_scatter=False)
+pydarn.RTP.plot_range_time(fitacf_data, parameter='elevation', beamnum=7,
+                           ground_scatter=False,
+                           norm_range=(0,57))
 plt.title("Elevation")
 plt.show()
