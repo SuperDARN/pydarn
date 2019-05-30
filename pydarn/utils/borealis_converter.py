5513881b29a91565f067d8c8fb073998ac846192
mardet987 <marci.detwiller@usask.ca>
2019-05-30 10:55:56 -0600

    Updated scripts for bzip2
    And added in multiprocessing to work in batches.

--- pydarn/utils/borealis_converter.py
+++ pydarn/utils/borealis_converter.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import argparse
+import bz2
 
 from pydarn import BorealisConvert
 
@@ -38,8 +39,8 @@ def borealis_conversion_parser():
 
 def create_dmap_filename(filename_to_convert, dmap_filetype):
     """
-    Creates a dmap filename in the same directory as the source file, to
-    write the DARN dmap file to.
+    Creates a dmap filename in the same directory as the source .hdf5 file, 
+    to write the DARN dmap file to.
     """
     basename = os.path.basename(filename_to_convert)
     basename_without_ext = '.'.join(basename.split('.')[0:-2]) # all but .rawacf.hdf5, for example.
@@ -47,7 +48,7 @@ def create_dmap_filename(filename_to_convert, dmap_filetype):
     return dmap_filename
 
 
-def borealis_converter(filename, dmap_filetype, dmap_filename):
+def borealis_write_to_dmap(filename, dmap_filetype, dmap_filename):
     """
     Convert the borealis file to Darn dmap filetype.
     """
@@ -60,15 +61,62 @@ def borealis_converter(filename, dmap_filetype, dmap_filename):
                             dmap_filename=dmap_filename))    
 
 
+def decompress_bz2(filename):
+    basename = os.path.basename(filename) 
+    newfilepath = os.path.dirname(filename) + '/' + '.'.join(basename.split('.')[0:-1]) # all but bz2
+
+    with open(newfilepath, 'wb') as new_file, bz2.BZ2File(filename, 'rb') as file:
+        for data in iter(lambda : file.read(100 * 1024), b''):
+            new_file.write(data)    
+
+    return newfilepath
+
+
+def compress_bz2(filename):
+    bz2_filename = filename + '.bz2'
+
+    with open(filename, 'rb') as file, bz2.BZ2File(bz2_filename, 'wb') as bz2_file:
+        for data in iter(lambda : file.read(100 * 1024), b''):
+            bz2_file.write(data)   
+
+    return bz2_filename
+
+
+def borealis_converter(filename, dmap_filetype, dmap_filename=None):
+    """
+    Checks if the file is bz2, decompresses if necessary, and 
+    writes to a dmap file. If the file was bz2, then the resulting dmap
+    file will also be compressed to bz2.
+    """
+
+    if os.path.basename(filename).split('.')[-1] in ['bz2', 'bzip2']:
+        borealis_hdf5_file = decompress_bz2(filename)
+        bzip2 = True
+    else:
+        borealis_hdf5_file = filename
+        bzip2 = False
+
+    if dmap_filename is None:
+        dmap_filename = create_dmap_filename(borealis_hdf5_file, dmap_filetype)
+
+    borealis_write_to_dmap(borealis_hdf5_file, dmap_filetype, dmap_filename)
+
+    if bzip2:
+        # remove the input file from the directory because it was generated.
+        os.remove(borealis_hdf5_file)
+        # compress the dmap file to bz2 if the input file was given as bz2.
+        bz2_filename = compress_bz2(dmap_filename)
+        os.remove(dmap_filename)
+        dmap_filename = bz2_filename
+
+    return dmap_filename
+
+
 def main():
     parser = borealis_conversion_parser()
     args = parser.parse_args()
 
-    if not args.dmap_filename:
-        dmap_filename = create_dmap_filename(args.borealis_hdf5_file, args.dmap_filetype)
-    else:
-        dmap_filename = args.dmap_filename
-    borealis_converter(args.borealis_hdf5_file, args.dmap_filetype, dmap_filename)
+    borealis_converter(args.borealis_hdf5_file, args.dmap_filetype, args.dmap_filename)
 
 
 if __name__ == "__main__":
