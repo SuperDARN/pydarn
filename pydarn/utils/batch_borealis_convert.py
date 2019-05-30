695bb09d694775783a457218588ef41189305372
mardet987 <marci.detwiller@usask.ca>
2019-05-30 10:55:56 -0600

    Updated scripts for bzip2
    And added in multiprocessing to work in batches.

--- pydarn/utils/batch_borealis_convert.py
+++ pydarn/utils/batch_borealis_convert.py
@@ -2,9 +2,10 @@ import sys
 import os
 import argparse
 import glob
-import subprocess
+from multiprocessing import Pool
+import bz2
 
-from borealis_converter import borealis_converter, create_dmap_filename
+from borealis_converter import borealis_converter
 
 def usage_msg():
     """
@@ -15,11 +16,14 @@ def usage_msg():
     :returns: the usage message
     """
 
-    usage_message = """ batch_borealis_convert.py [-h] directory_to_convert
+    usage_message = """ batch_borealis_convert.py [-h] -bz2 directory_to_convert 
     
     Pass in the directory you wish to convert. Filenames with .bfiq.hdf5 will be attempted to be 
     converted to iqdat dmap. Filenames with .rawacf.hdf5 will be attempted to be converted to 
     rawacf dmap.
+
+    Filenames with .rawacf.hdf5.bz2 or .bfiq.hdf5.bz2 will be decompressed and then converted. The
+    converted file will then be bzipped if the input file was bzipped.
 
     The script will convert the hdf5 files from the directory to file as the given filename,
     with extension replaced with new dmap_filetype."""
@@ -31,26 +35,33 @@ def borealis_conversion_parser():
     parser = argparse.ArgumentParser(usage=usage_msg())
     parser.add_argument("directory_to_convert", help="Path to the files you wish to convert to "
                                                    "SuperDARN dmap type.")
-
     return parser
 
 
-def main():
-    parser = borealis_conversion_parser()
-    args = parser.parse_args()
+def rawacf_borealis_converter(rawacf_file):
 
-    rawacf_hdf5_files = glob.glob(args.directory_to_convert + '*.rawacf.hdf5')
-    for rawacf_file in rawacf_hdf5_files:
-        dmap_filename = create_dmap_filename(rawacf_file, "rawacf")
-        borealis_converter(rawacf_file, "rawacf", dmap_filename)
-    print("Successfully converted all rawacf files in directory.")
+    rawacf_dmap_file = borealis_converter(rawacf_file, "rawacf")
 
-    bfiq_hdf5_files = glob.glob(args.directory_to_convert + '*bfiq.hdf5')
-    for bfiq_file in bfiq_hdf5_files:
-        dmap_filename = create_dmap_filename(bfiq_file, "iqdat")
-        borealis_converter(bfiq_file, "iqdat", dmap_filename)
-    print("Successfully converted all bfiq files in directory.")
+
+def bfiq_borealis_converter(bfiq_file):
+
+    bfiq_dmap_file = borealis_converter(bfiq_file, "iqdat")
 
 
 if __name__ == "__main__":
+    parser = borealis_conversion_parser()
+    args = parser.parse_args()
+
+    rawacf_pool = Pool(processes=4)
+    bfiq_pool = Pool(processes=4)
+
+    rawacf_hdf5_files = glob.glob(args.directory_to_convert + '*.rawacf.hdf5')
+    rawacf_hdf5_files.extend(glob.glob(args.directory_to_convert + '*.rawacf.hdf5.bz2'))
+
+    bfiq_hdf5_files = glob.glob(args.directory_to_convert + '*bfiq.hdf5')
+    bfiq_hdf5_files.extend(glob.glob(args.directory_to_convert + '*bfiq.hdf5.bz2'))
+    
+    rawacf_pool.map(rawacf_borealis_converter, rawacf_hdf5_files)
+    bfiq_pool.map(bfiq_borealis_converter, bfiq_hdf5_files)
+
     main()
