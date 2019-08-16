314cd2efb53d046c7f1f6f14ea7553b79b884468
mardet987 <marci.detwiller@usask.ca>
2019-08-16 14:49:46 -0600

    Moved convert explicit function
    Moved out of util file into borealis.py and also made explicit
    functions bfiq2iqdatdmap and rawacf2rawacfdmap.
    
    In the process of this created a new exception in case the
    user tries to convert to the same filename as source file.

--- pydarn/io/borealis.py
+++ pydarn/io/borealis.py
@@ -22,6 +22,7 @@ BorealisDataFormatTypeError
 BorealisConversionTypesError
 BorealisConvert2IqdatError
 BorealisConvert2RawacfError
+ConvertFileOverWriteError
 
 Future work
 -----------
@@ -31,6 +32,13 @@ when Borealis files become restructured to save storage space.
 Notes
 -----
 BorealisConvert makes use of DarnWrite to write to SuperDARN file types
+
+See Also
+--------
+
+For more information on Borealis data files and how they convert to dmap,
+see: https://borealis.readthedocs.io/en/latest/ 
+
 """
 import deepdish as dd
 import h5py
@@ -43,7 +51,8 @@ from collections import OrderedDict
 from datetime import datetime
 from typing import Union, List
 
-from pydarn import borealis_exceptions, DarnWrite, borealis_formats
+from pydarn import borealis_exceptions, DarnWrite, borealis_formats, 
+                    dict2dmap
 # from pydarn.utils.conversions import dict2dmap
 
 pydarn_log = logging.getLogger('pydarn')
@@ -1331,3 +1340,144 @@ class BorealisConvert():
 
         dmap_recs = dict2dmap(recs)
         return dmap_recs
+
+
+def borealis_write_to_dmap(filename, dmap_filetype, dmap_filename):
+    """
+    Convert the borealis file to DARN dmap filetype.
+
+    Parameters
+    ----------
+    filename: str
+        Name of the file where Borealis hdf5 data is stored, to read from.
+    dmap_filetype: str
+        Type of DARN dmap filetype you would like to convert to. Possible
+        types include 'iqdat' and 'rawacf'. Borealis 'bfiq' can be converted 
+        to iqdat, and Borealis 'rawacf' can be converted to DARN rawacf.
+    dmap_filename: str
+        Name of the file that you want to save the DARN dmap file to. 
+
+    Raises
+    ------
+    BorealisFileTypeError
+    BorealisFieldMissingError
+    BorealisExtraFieldError
+    BorealisDataFormatTypeError
+    BorealisConversionTypesError
+    BorealisConvert2IqdatError
+    BorealisConvert2RawacfError
+
+    See Also
+    --------
+    BorealisConvert 
+        Class that is used to convert
+    BorealisRead
+        BorealisConvert uses this
+    DarnWrite
+        BorealisConvert uses this
+    """
+    borealis_data = BorealisConvert(filename)
+    pydarn_log.debug('Read the file {filename}'.format(filename=filename))
+    dmap_filename = borealis_data.write_to_dmap(dmap_filetype, dmap_filename)
+
+    pydarn_log.debug("Borealis file {filename} written to {dmap_filename} "
+          "without errors.".format(filename=borealis_data.filename, 
+                          dmap_filename=dmap_filename))    
+
+
+def bfiq2iqdatdmap(filename, **kwargs):
+    """
+    Convert the borealis bfiq hdf5 file to DARN iqdat filetype.
+
+    Parameters
+    ----------
+    filename: str
+        Name of the file where Borealis bfiq hdf5 data is stored, to read from.
+    dmap_filename: str
+        Name of the file that you want to save the DARN dmap file to. 
+
+    Raises
+    ------
+    BorealisFileTypeError
+    BorealisFieldMissingError
+    BorealisExtraFieldError
+    BorealisDataFormatTypeError
+    BorealisConversionTypesError
+    BorealisConvert2IqdatError
+    BorealisConvert2RawacfError
+    ConvertFileOverWriteError
+
+    See Also
+    --------
+    borealis_write_to_dmap
+    """
+
+    settings = {}
+
+    # put dmap in place of hdf5 in each spot (usually just 1)
+    dmap_filename = os.path.dirname(filename) + '/'
+    basename_partials = os.path.basename(filename).split("hdf5")
+    for num, basename_partial in enumerate(basename_partials):
+        if num != len(basename_partials) - 1:
+            dmap_filename += basename_partial + 'dmap'
+        else: # last partial, no 'dmap' after.
+            dmap_filename += basename_partial
+    
+    # set defaults
+    settings['dmap_filename'] = dmap_filename
+    settings.update(kwargs)
+
+    # verify not converting to the same filename.
+    if settings['dmap_filename'] == filename:
+        raise ConvertFileOverWriteError(filename)
+    
+    borealis_write_to_dmap(filename, 'bfiq', settings['dmap_filename'])
+
+
+def rawacf2rawacfdmap(filename, **kwargs):
+    """
+    Convert the borealis rawacf hdf5 file to DARN rawacf filetype.
+
+    Parameters
+    ----------
+    filename: str
+        Name of the file where Borealis rawacf hdf5 data is stored, to read from.
+    dmap_filename: str
+        Name of the file that you want to save the DARN dmap file to. 
+
+    Raises
+    ------
+    BorealisFileTypeError
+    BorealisFieldMissingError
+    BorealisExtraFieldError
+    BorealisDataFormatTypeError
+    BorealisConversionTypesError
+    BorealisConvert2IqdatError
+    BorealisConvert2RawacfError
+    ConvertFileOverWriteError
+
+    See Also
+    --------
+    borealis_write_to_dmap
+    """
+
+    settings = {}
+
+    # put dmap in place of hdf5 in each spot (usually just 1)
+    dmap_filename = os.path.dirname(filename) + '/'
+    basename_partials = os.path.basename(filename).split("hdf5")
+    for num, basename_partial in enumerate(basename_partials):
+        if num != len(basename_partials) - 1:
+            dmap_filename += basename_partial + 'dmap'
+        else: # last partial, no 'dmap' after.
+            dmap_filename += basename_partial
+    
+    # set defaults
+    settings['dmap_filename'] = dmap_filename
+    settings.update(kwargs)
+
+    # verify not converting to the same filename.
+    if settings['dmap_filename'] == filename:
+        raise ConvertFileOverWriteError(filename)
+    
+    borealis_write_to_dmap(filename, 'rawacf', settings['dmap_filename'])
