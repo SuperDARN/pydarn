9c0365d3a2ef20ddcf7bff82d8cec8e3bb28c5e8
mardet987 <marci.detwiller@usask.ca>
2019-09-03 15:52:32 -0600

    Read/write changes for changes to restructuring
    A couple of fields only belong to either array style or record (site)
    style files now so the formats and read/write checks had to be adjusted.
    
    Also had to move a few fields from shared_fields to unshared_fields
    after a bug was found with restructuring.

--- pydarn/io/borealis/borealis_formats.py
+++ pydarn/io/borealis/borealis_formats.py
@@ -46,17 +46,44 @@ class BorealisRawacf():
         in the field for a dictionary of Borealis data.
     shared_fields: list
         List of the fields that are restructured to a single value per
-        file in the Borealis array type files. 
+        file in the Borealis array type files. These fields are present in both
+        array and site files. 
     unshared_fields: list
         List of the fields that are restructured to be an array with first
-        dimension equal to the number of records in the file. 
+        dimension equal to the number of records in the file. These fields are 
+        present in both array and site files. 
+    array_only_fields: list
+        List of fields that are only present in array files.
+    site_only_fields: list
+        List of fields that are only present in site files.
+    site_fields: list
+        List of all fields that are in the site file type.
+    array_fields: list 
+        List of all fields that are in the array file type. 
+    site_single_element_fields : list
+        List of fields in the site files that are single element types. 
+    site_single_element_types: dict
+        subset of single_element_types with only site keys.
+    site_array_dtypes_fields : list
+        List of fields in the site files that are made of numpy arrays. 
+    site_array_dtypes: dict
+        subset of array_dtypes with only site keys.
+    array_single_element_fields : list
+        List of fields in the array files that are single element types 
+        (unless unshared in which they are converted to the numpy array of 
+        that type, with number of records elements)
+    array_single_element_types: dict
+        subset of single_element_types with only array keys.
+    array_array_dtypes_fields : list
+        List of fields in the array files that are made of numpy arrays. 
+    array_array_dtypes: dict
+        subset of array_dtypes with only array keys.
 
     Notes
     -----
     single_element_types.keys() + array_dtypes.keys() = all known fields
-    as well as 
-    shared_fields + unshared_fields = all known fields
-    in the Borealis Rawacf files.
+    shared_fields + unshared_fields + array_only_fields = all fields in array file
+    shared_fields + unshared_fields + site_only_fields = all fields in site file
     """
 
     single_element_types = {
@@ -104,7 +131,9 @@ class BorealisRawacf():
         "samples_data_type": np.unicode_,
         # data normalization factor determined by the filter scaling in the 
         # decimation scheme.
-        "data_normalization_factor": np.float64
+        "data_normalization_factor": np.float64,
+        # number of beams calculated for the integration time.
+        "num_beams": np.uint32
     }
 
     array_dtypes = {
@@ -126,7 +155,8 @@ class BorealisRawacf():
         # FFT resolution bandwidth for this value, should be = output_sample 
         # rate?)
         "noise_at_freq": np.float64,
-        # Denotes what each acf/xcf dimension represents.
+        # Denotes what each acf/xcf dimension represents. = "num_beams", 
+        # "num_ranges", "num_lags" in site rawacf files.
         "correlation_descriptors": np.unicode_,
         # The dimensions in which to reshape the acf/xcf data.
         "correlation_dimensions": np.uint32,
@@ -138,18 +168,52 @@ class BorealisRawacf():
         "xcfs": np.complex64 
     }
 
-    shared_fields = ['beam_azms', 'beam_nums', 'blanked_samples', 
-                     'borealis_git_hash', 'correlation_descriptors',
+    shared_fields = ['blanked_samples', 'borealis_git_hash', 
+                     'correlation_descriptors',
                      'data_normalization_factor', 'experiment_comment', 
                      'experiment_id', 'experiment_name', 'first_range', 
                      'first_range_rtt', 'freq', 'intf_antenna_count', 'lags', 
-                     'main_antenna_count', 'num_slices', 'pulses', 'range_sep',
+                     'main_antenna_count', 'pulses', 'range_sep',
                      'rx_sample_rate', 'samples_data_type', 
-                     'scan_start_marker', 'slice_comment', 'station', 
-                     'tau_spacing', 'tx_pulse_len', 'correlation_dimensions']
+                     'slice_comment', 'station', 'tau_spacing', 
+                     'tx_pulse_len']
 
     unshared_fields = ['num_sequences', 'int_time', 'sqn_timestamps',
-                       'noise_at_freq', 'main_acfs', 'intf_acfs', 'xcfs']
+                       'noise_at_freq', 'main_acfs', 'intf_acfs', 'xcfs',
+                       'scan_start_marker', 'beam_nums', 'beam_azms', 
+                       'num_slices']
+
+    array_only_fields = ['num_beams']
+
+    site_only_fields = ['correlation_dimensions']
+
+    site_fields = shared_fields + unshared_fields + site_only_fields
+
+    array_fields = shared_fields + unshared_fields + array_only_fields
+
+    site_single_element_fields = [k for k in site_fields if k in 
+        list(single_element_types.keys())]
+
+    site_single_element_types = {k: single_element_types[k] for k in 
+        site_single_element_fields}
+
+    site_array_dtypes_fields = [k for k in site_fields if k in 
+        list(array_dtypes.keys())]
+
+    site_array_dtypes = {k: array_dtypes[k] for k in 
+        site_array_dtypes_fields}
+
+    array_single_element_fields = [k for k in array_fields if k in 
+        list(single_element_types.keys())]
+
+    array_single_element_types = {k: single_element_types[k] for k in 
+        array_single_element_fields}
+
+    array_array_dtypes_fields = [k for k in array_fields if k in 
+        list(array_dtypes.keys())]
+
+    array_array_dtypes = {k: array_dtypes[k] for k in 
+        array_array_dtypes_fields}
 
 
 class BorealisBfiq():
@@ -171,17 +235,44 @@ class BorealisBfiq():
         in the field for a dictionary of Borealis data.
     shared_fields: list
         List of the fields that are restructured to a single value per
-        file in the Borealis array type files. 
+        file in the Borealis array type files. These fields are present in both
+        array and site files. 
     unshared_fields: list
         List of the fields that are restructured to be an array with first
-        dimension equal to the number of records in the file. 
-    
+        dimension equal to the number of records in the file. These fields are 
+        present in both array and site files. 
+    array_only_fields: list
+        List of fields that are only present in array files.
+    site_only_fields: list
+        List of fields that are only present in site files.
+    site_fields: list
+        List of all fields that are in the site file type.
+    array_fields: list 
+        List of all fields that are in the array file type. 
+    site_single_element_fields : list
+        List of fields in the site files that are single element types. 
+    site_single_element_types: dict
+        subset of single_element_types with only site keys.
+    site_array_dtypes_fields : list
+        List of fields in the site files that are made of numpy arrays. 
+    site_array_dtypes: dict
+        subset of array_dtypes with only site keys.
+    array_single_element_fields : list
+        List of fields in the array files that are single element types 
+        (unless unshared in which they are converted to the numpy array of 
+        that type, with number of records elements)
+    array_single_element_types: dict
+        subset of single_element_types with only array keys.
+    array_array_dtypes_fields : list
+        List of fields in the array files that are made of numpy arrays. 
+    array_array_dtypes: dict
+        subset of array_dtypes with only array keys.
+
     Notes
     -----
     single_element_types.keys() + array_dtypes.keys() = all known fields
-    as well as 
-    shared_fields + unshared_fields = all known fields
-    in the Borealis Bfiq files.
+    shared_fields + unshared_fields + array_only_fields = all fields in array file
+    shared_fields + unshared_fields + site_only_fields = all fields in site file
     """
 
     single_element_types = {
@@ -233,7 +324,9 @@ class BorealisBfiq():
         "num_ranges": np.uint32,
         # data normalization factor determined by the filter scaling in the 
         # decimation scheme.
-        "data_normalization_factor": np.float64
+        "data_normalization_factor": np.float64,
+        # number of beams calculated for the integration time.
+        "num_beams": np.uint32
     }
 
     array_dtypes = {
@@ -259,7 +352,8 @@ class BorealisBfiq():
         "noise_at_freq": np.float64,
         # States what order the data is in. Describes the data layout.
         "antenna_arrays_order": np.unicode_,
-        # Denotes what each data dimension represents.
+        # Denotes what each data dimension represents. = "num_antenna_arrays",
+        # "num_sequences", "num_beams", "num_samps" for site bfiq.
         "data_descriptors": np.unicode_,
         # The dimensions in which to reshape the data.
         "data_dimensions": np.uint32,
@@ -267,20 +361,53 @@ class BorealisBfiq():
         "data": np.complex64
     }
 
-    shared_fields =  ['antenna_arrays_order', 'beam_azms', 'beam_nums',
+    shared_fields =  ['antenna_arrays_order', 
                       'blanked_samples', 'borealis_git_hash', 
                       'data_descriptors', 'data_normalization_factor', 
                       'experiment_comment', 'experiment_id', 'experiment_name',
                       'first_range', 'first_range_rtt', 'freq', 
                       'intf_antenna_count', 'lags', 'main_antenna_count', 
-                      'num_ranges', 'num_samps', 'num_slices', 
+                      'num_ranges', 'num_samps',  
                       'pulse_phase_offset', 'pulses', 'range_sep', 
                       'rx_sample_rate', 'samples_data_type', 
-                      'scan_start_marker', 'slice_comment', 'station', 
-                      'tau_spacing', 'tx_pulse_len']
+                      'slice_comment', 'station', 'tau_spacing', 
+                      'tx_pulse_len']
 
     unshared_fields = ['num_sequences', 'int_time', 'sqn_timestamps',
-                       'noise_at_freq', 'data_dimensions', 'data']
+                       'noise_at_freq', 'data', 'scan_start_marker', 
+                       'beam_azms', 'beam_nums', 'num_slices']
+
+    array_only_fields = ['num_beams']
+
+    site_only_fields = ['data_dimensions']
+
+    site_fields = shared_fields + unshared_fields + site_only_fields
+
+    array_fields = shared_fields + unshared_fields + array_only_fields
+
+    site_single_element_fields = [k for k in site_fields if k in 
+        list(single_element_types.keys())]
+
+    site_single_element_types = {k: single_element_types[k] for k in 
+        site_single_element_fields}
+
+    site_array_dtypes_fields = [k for k in site_fields if k in 
+        list(array_dtypes.keys())]
+
+    site_array_dtypes = {k: array_dtypes[k] for k in 
+        site_array_dtypes_fields}
+
+    array_single_element_fields = [k for k in array_fields if k in 
+        list(single_element_types.keys())]
+
+    array_single_element_types = {k: single_element_types[k] for k in 
+        array_single_element_fields}
+
+    array_array_dtypes_fields = [k for k in array_fields if k in 
+        list(array_dtypes.keys())]
+
+    array_array_dtypes = {k: array_dtypes[k] for k in 
+        array_array_dtypes_fields}
 
 
 class BorealisAntennasIq():
@@ -301,17 +428,44 @@ class BorealisAntennasIq():
         in the field for a dictionary of Borealis data.
     shared_fields: list
         List of the fields that are restructured to a single value per
-        file in the Borealis array type files. 
+        file in the Borealis array type files. These fields are present in both
+        array and site files. 
     unshared_fields: list
         List of the fields that are restructured to be an array with first
-        dimension equal to the number of records in the file. 
-    
+        dimension equal to the number of records in the file. These fields are 
+        present in both array and site files. 
+    array_only_fields: list
+        List of fields that are only present in array files.
+    site_only_fields: list
+        List of fields that are only present in site files.
+    site_fields: list
+        List of all fields that are in the site file type.
+    array_fields: list 
+        List of all fields that are in the array file type. 
+    site_single_element_fields : list
+        List of fields in the site files that are single element types. 
+    site_single_element_types: dict
+        subset of single_element_types with only site keys.
+    site_array_dtypes_fields : list
+        List of fields in the site files that are made of numpy arrays. 
+    site_array_dtypes: dict
+        subset of array_dtypes with only site keys.
+    array_single_element_fields : list
+        List of fields in the array files that are single element types 
+        (unless unshared in which they are converted to the numpy array of 
+        that type, with number of records elements)
+    array_single_element_types: dict
+        subset of single_element_types with only array keys.
+    array_array_dtypes_fields : list
+        List of fields in the array files that are made of numpy arrays. 
+    array_array_dtypes: dict
+        subset of array_dtypes with only array keys.
+
     Notes
     -----
     single_element_types.keys() + array_dtypes.keys() = all known fields
-    as well as 
-    shared_fields + unshared_fields = all known fields
-    in the Borealis Antennas_iq files.
+    shared_fields + unshared_fields + array_only_fields = all fields in array file
+    shared_fields + unshared_fields + site_only_fields = all fields in site file
     """
 
     single_element_types = {
@@ -355,7 +509,9 @@ class BorealisAntennasIq():
         "num_samps": np.uint32,  
         # data normalization factor determined by the filter scaling in the 
         # decimation scheme.
-        "data_normalization_factor": np.float64
+        "data_normalization_factor": np.float64,
+        # number of beams to be calculated for the integration time.
+        "num_beams": np.uint32
     }
 
     array_dtypes = {
@@ -377,7 +533,8 @@ class BorealisAntennasIq():
         "noise_at_freq": np.float64,
         # States what order the data is in. Describes the data layout.
         "antenna_arrays_order": np.unicode_,
-        # Denotes what each data dimension represents.
+        # Denotes what each data dimension represents. = "num_antennas", 
+        # "num_sequences", "num_samps" in site antennas_iq.
         "data_descriptors": np.unicode_,
         # The dimensions in which to reshape the data.
         "data_dimensions": np.uint32,
@@ -385,23 +542,58 @@ class BorealisAntennasIq():
         "data": np.complex64
     }
 
-    shared_fields = ['antenna_arrays_order', 'beam_azms', 'beam_nums', 
+    shared_fields = ['antenna_arrays_order', 
                      'borealis_git_hash', 'data_descriptors',
                      'data_normalization_factor', 'experiment_comment', 
                      'experiment_id', 'experiment_name', 'freq', 
                      'intf_antenna_count', 'main_antenna_count', 'num_samps',
-                     'num_slices', 'pulse_phase_offset', 'pulses', 
+                     'pulse_phase_offset', 'pulses', 
                      'rx_sample_rate', 'samples_data_type', 
-                     'scan_start_marker', 'slice_comment', 'station', 
-                     'tau_spacing', 'tx_pulse_len']
+                     'slice_comment', 'station', 'tau_spacing', 
+                     'tx_pulse_len']
 
     unshared_fields = ['num_sequences', 'int_time', 'sqn_timestamps',
-                       'noise_at_freq', 'data_dimensions', 'data']
+                       'noise_at_freq', 'data', 'scan_start_marker', 
+                       'beam_azms', 'beam_nums', 'num_slices']
+
+    array_only_fields = ['num_beams']
+
+    site_only_fields = ['data_dimensions']
+
+    site_fields = shared_fields + unshared_fields + site_only_fields
+
+    array_fields = shared_fields + unshared_fields + array_only_fields
+
+    site_single_element_fields = [k for k in site_fields if k in 
+        list(single_element_types.keys())]
+
+    site_single_element_types = {k: single_element_types[k] for k in 
+        site_single_element_fields}
+
+    site_array_dtypes_fields = [k for k in site_fields if k in 
+        list(array_dtypes.keys())]
+
+    site_array_dtypes = {k: array_dtypes[k] for k in 
+        site_array_dtypes_fields}
+
+    array_single_element_fields = [k for k in array_fields if k in 
+        list(single_element_types.keys())]
+
+    array_single_element_types = {k: single_element_types[k] for k in 
+        array_single_element_fields}
+
+    array_array_dtypes_fields = [k for k in array_fields if k in 
+        list(array_dtypes.keys())]
+
+    array_array_dtypes = {k: array_dtypes[k] for k in 
+        array_array_dtypes_fields}
 
 
 class BorealisRawrf():
     """
-    Class containing Borealis Rawrf data fields and their types.
+    Class containing Borealis Rawrf data fields and their types. 
+
+    This filetype only has site files and cannot be restructured.
 
     Rawrf data is data that has been produced at the original receive bandwidth
     and has not been mixed, filtered, or decimated. 
@@ -414,6 +606,8 @@ class BorealisRawrf():
     array_dtypes: dict
         Dictionary of data field name to array of given numpy dtype expected  
         in the field for a dictionary of Borealis data.
+    site_fields: list
+        List of all fields that are in the site file type.
     """
 
     single_element_types = {
@@ -454,10 +648,13 @@ class BorealisRawrf():
         # A list of GPS timestamps of the beginning of transmission for each
         # sampling period in the integration time. Seconds since epoch.
         "sqn_timestamps": np.float64,
-        # Denotes what each data dimension represents.
+        # Denotes what each data dimension represents. = "num_sequences", 
+        # "num_antennas", "num_samps" for rawrf.
         "data_descriptors": np.unicode_,
         # The dimensions in which to reshape the data.
         "data_dimensions": np.uint32,
         # A contiguous set of samples (complex float) at given sample rate
         "data": np.complex64
     }
+
+    site_fields = list(single_element_types.keys()) + list(array_dtypes.keys())
