7b9570a554cd77ea0a0d655dd03b35e0aed4a9aa
Marina Schmidt <marina@auroral.usask.ca>
2020-01-15 13:35:42 -0600

    added testing comments

--- test/unit/test_utils.py
+++ test/unit/test_utils.py
@@ -15,6 +15,10 @@ class TestSuperDarnRadars(unittest.TestCase):
     This class tests the superdarn_radars module
     """
     def test_read_hdw_file(self):
+        """
+        test if read_hdw_file can read Saskatoon's
+        most current hardware information
+        """
         hdw_data = pydarn.read_hdw_file('sas')
         self.assertEqual(hdw_data.abbrev, 'sas')
         self.assertEqual(hdw_data.stid, 5)
@@ -36,6 +40,10 @@ class TestSuperDarnRadars(unittest.TestCase):
         self.assertEqual(hdw_data.beams, 16)
 
     def test_read_hdw_file_old_year(self):
+        """
+        test if read_hdw_file can read McMurdo's older
+        hardware file information
+        """
         hdw_data = pydarn.read_hdw_file('mcm', datetime(year=2016, month=1,
                                                         day=1))
         self.assertEqual(hdw_data.abbrev, 'mcm')
@@ -58,22 +66,38 @@ class TestSuperDarnRadars(unittest.TestCase):
         self.assertEqual(hdw_data.beams, 16)
 
     def test_hardware_file_not_found(self):
+        """
+        Test if it raises an error for an non-existing
+        hardware file
+        """
         with self.assertRaises(pydarn.radar_exceptions.HardwareFileNotFoundError):
             pydarn.read_hdw_file('dog')
 
     def test_SuperDarn_class_dict(self):
+        """
+        test if the superdarn class dict returns
+        Goose Bay's radars information
+        """
         radar_data = pydarn.SuperDARNRadars.radars[1]
         self.assertEqual(radar_data.institution, 'Virginia Tech')
         self.assertEqual(radar_data.name, 'Goose Bay')
         self.assertEqual(radar_data.hemisphere, pydarn.Hemisphere.North)
 
     def test_SuperDARN_class_south_radar(self):
+        """
+        test if SuperDARN radars returns a southern
+        hemisphere radar information correctly
+        """
         radar_data = pydarn.SuperDARNRadars.radars[24]
         self.assertEqual(radar_data.institution, 'La Trobe University')
         self.assertEqual(radar_data.name, 'Buckland Park')
         self.assertEqual(radar_data.hemisphere, pydarn.Hemisphere.South)
 
     def test_SuperDarn_class_stid_not_found(self):
+        """
+        Test if it raises an error for a radar stid
+        that does not exist
+        """
         with self.assertRaises(KeyError):
             pydarn.SuperDARNRadars.radars[100]
 
