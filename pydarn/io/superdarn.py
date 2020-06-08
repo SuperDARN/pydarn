305b524e37dca9082a6700089458cc6ba78b0f35
mts299 <mts299888@gmail.com>
2020-06-08 16:33:54 -0600

    redoing logging levels in pydarn

--- pydarn/io/superdarn.py
+++ pydarn/io/superdarn.py
@@ -396,7 +396,7 @@ class SDarnRead(DmapRead):
         dmap_records : List[dict]
             DMAP record of the Iqdat data
         """
-        pydarn_log.debug("Reading Iqdat file: {}".format(self.dmap_file))
+        pydarn_log.info("Reading Iqdat file: {}".format(self.dmap_file))
         file_struct_list = [superdarn_formats.Iqdat.types]
         self._read_darn_records(file_struct_list)
         self.records = dmap2dict(self._dmap_records)
@@ -411,7 +411,7 @@ class SDarnRead(DmapRead):
         dmap_records : List[dict]
             DMAP record of the Rawacf data
         """
-        pydarn_log.debug("Reading Rawacf file: {}".format(self.dmap_file))
+        pydarn_log.info("Reading Rawacf file: {}".format(self.dmap_file))
 
         file_struct_list = [superdarn_formats.Rawacf.types,
                             superdarn_formats.Rawacf.extra_fields,
@@ -429,7 +429,7 @@ class SDarnRead(DmapRead):
         dmap_records : List[dict]
             DMAP record of the Fitacf data
         """
-        pydarn_log.debug("Reading Fitacf file: {}".format(self.dmap_file))
+        pydarn_log.info("Reading Fitacf file: {}".format(self.dmap_file))
 
         # We need to separate the fields into subsets because fitacf fitting
         # methods 2.5 and 3.0 do not include a subset of fields if the data
@@ -452,7 +452,7 @@ class SDarnRead(DmapRead):
         dmap_records : List[dict]
             DMAP record of the Grid data
         """
-        pydarn_log.debug("Reading Grid file: {}".format(self.dmap_file))
+        pydarn_log.info("Reading Grid file: {}".format(self.dmap_file))
         # We need to separate the fields into subsets because grid files
         # can exclude extra fields if the option -ext is not passed in
         # when generating the grid file in RST. See missing_field_check
@@ -473,7 +473,7 @@ class SDarnRead(DmapRead):
         dmap_records : List[dict]
             DMAP record of the Map data
         """
-        pydarn_log.debug("Reading Map file: {}".format(self.dmap_file))
+        pydarn_log.info("Reading Map file: {}".format(self.dmap_file))
         # We need to separate the fields into subsets because map files
         # can exclude extra fields if the grid file does not contain them.
         # Other subsets are related to what processing commands are used to
@@ -580,7 +580,7 @@ class SDarnWrite(DmapWrite):
         """
         self._filename_check(filename)
         self._empty_record_check()
-        pydarn_log.debug("Writing Iqdat file: {}".format(self.filename))
+        pydarn_log.info("Writing Iqdat file: {}".format(self.filename))
 
         file_struct_list = [superdarn_formats.Iqdat.types]
         self.superDARN_file_structure_to_bytes(file_struct_list)
@@ -611,7 +611,7 @@ class SDarnWrite(DmapWrite):
         superdarn_formats.Rawacf - module contain the data types
                                  in each SuperDARN files types
         """
-        pydarn_log.debug("Writing Rawacf file: {}".format(self.filename))
+        pydarn_log.info("Writing Rawacf file: {}".format(self.filename))
         self._filename_check(filename)
         self._empty_record_check()
         file_struct_list = [superdarn_formats.Rawacf.types,
@@ -645,7 +645,7 @@ class SDarnWrite(DmapWrite):
         superdarn_formats.Fitacf - module contain the data types
                                  in each SuperDARN files types
         """
-        pydarn_log.debug("Writing Fitacf file: {}".format(self.filename))
+        pydarn_log.info("Writing Fitacf file: {}".format(self.filename))
 
         self._filename_check(filename)
         self._empty_record_check()
@@ -685,7 +685,7 @@ class SDarnWrite(DmapWrite):
         superdarn_formats.Grid - module contain the data types
                                  in each SuperDARN files types
         """
-        pydarn_log.debug("Writing Grid file: {}".format(self.filename))
+        pydarn_log.info("Writing Grid file: {}".format(self.filename))
 
         self._filename_check(filename)
         self._empty_record_check()
@@ -724,7 +724,7 @@ class SDarnWrite(DmapWrite):
         superdarn_formats.Map - module contain the data types
                                  in each SuperDARN files types
         """
-        pydarn_log.debug("Writing Map file: {}".format(self.filename))
+        pydarn_log.info("Writing Map file: {}".format(self.filename))
 
         self._filename_check(filename)
         self._empty_record_check()
