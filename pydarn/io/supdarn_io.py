0e48f8d7cba9abb5be5b596d6e76b4f638a1427a
mts299 <marina.t.schmidt@gmail.com>
2020-10-05 12:07:14 -0600

    Adding io reading methods for pyDARN plotting
    In the user case, users want to read and plot pyDARN inherits from
    pydarnio to use its reading ability.
    
    Currently dmap works, still need to figure things out with Borealis

---
+++ pydarn/io/supdarn_io.py
@@ -0,0 +1,110 @@
+# Copyright (C) 2020 SuperDARN Canada, University of Saskatchewan
+# Author: Marina Schmidt
+
+import bz2
+import pydarnio
+import os
+
+
+class SuperDARNRead(pydarnio.SDarnRead):
+    """
+    A class that reads select SuperDARN files for pyDARN plotting
+
+    Methods
+    -------
+    read_dmap : reads superDARN DMap formats
+    read_borealis: not implemented yet
+    """
+    def __init__(self):
+        pass
+
+    def read_dmap(self, filename: str):
+        """
+        Reads select SuperDARN DMap files for pyDARN plotting:
+            fitacf
+            rawacf
+            iqdat
+        When more plotting methods are introduced other files
+        will be included.
+
+        Parameters
+        ----------
+            filename: string
+                name of the file you are going to read
+
+        Returns
+        ------
+            data: List[dict]
+                data records from the file
+        """
+        # check if the file  is compressed with
+        # bz2
+        if 'bz2' in filename:
+            with bz2.open(filename) as fp:
+                dmap_stream = fp.read()
+
+            super().__init__(dmap_stream, stream=True)
+        else:
+            super().__init__(filename)
+
+        try:
+            # Check which file type it is
+            if 'rawacf' in filename:
+                data = self.read_rawacf()
+            elif 'fitacf' in filename:
+                data = self.read_fitacf()
+            elif 'iqdat' in filename:
+                self.read_iqdat()
+            # if not noticeable then just read the file
+            else:
+                data = self.read_records()
+        except Exception as err:
+            # sometimes files might fail due to issues with
+            # the specific reading methods. This is not a pyDARN
+            # issue so we will just read the dmap file if that fails
+            # then make an issue on pyDARNio
+            print(err)
+            print("..... Will try to read DMap file with read_dmap")
+            print(" IF THIS FAILS please make an issue on pyDARNio, not "
+                  "pyDARN's issue")
+            data = self.read_records
+        return data
+
+    def read_borealis(self, filename: str, slice_id: int = None):
+        """
+        Reads RAWACF or BFIQ borealis files and converts them to
+        pyDARN data format for plotting.
+
+        Parameters
+        ----------
+            filename: str
+                string name of the file, make sure "rawacf" or "bfiq" is in
+                the name
+
+        Post
+        ----
+            Produces a file named: dmap_file.rawacf, this should be deleted
+            at the end of the file. Requirements for BorealisConvert.
+
+        Raises
+        -----
+            ValueError - if the file type is not determined in the filename
+            then it raises an error that it cannot convert the file.
+        """
+        new_filename = 'dmap_file.rawacf'
+
+        if 'rawacf' in filename:
+            file_type = 'rawacf'
+        elif 'bfiq' in filename:
+            file_type = 'bfiq'
+        else:
+            raise ValueError("{} cannot  be converted to borealis"
+                             " please make sure the filename has rawacf"
+                             " or bfiq in the name or look up the accepted"
+                             " file in pyDARNio's"
+                             " documentation".format(filename))
+
+        converter = pydarnio.BorealisConvert(filename, file_type,
+                                             new_filename, slice_id)
+        os.remove(new_filename)
+        return converter.sdarn_dict
