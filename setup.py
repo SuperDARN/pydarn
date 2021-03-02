68f764f0b9960ae16a35e37825ccb0e7bd605a95
mts299 <marina.t.schmidt@gmail.com>
2021-03-02 13:40:33 -0600

    adding cmocean

--- setup.py
+++ setup.py
@@ -74,5 +74,5 @@ setup(
     include_package_data=True,
     # setup_requires=['pyyaml', 'numpy', 'matplotlib', 'aacgmv2'],
     install_requires=['pyyaml', 'numpy', 'matplotlib', 'aacgmv2',
-                      'pydarnio'],
+                      'pydarnio', 'cmocean'],
 )
