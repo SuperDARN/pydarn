d7bd61987c15b531b1fb7a45d2b15b881e9cbd1a
francistee26 <64978777+francistee26@users.noreply.github.com>
2021-04-28 14:06:12 -0400

    Update pydarn/plotting/rtp.py
    Co-authored-by: carleyjmartin <60905856+carleyjmartin@users.noreply.github.com>

--- pydarn/plotting/rtp.py
+++ pydarn/plotting/rtp.py
@@ -437,6 +437,8 @@ class RTP():
         ax.xaxis.set_minor_locator(dates.MinuteLocator(interval=tick_interval))
         if coord is Coords.SLANT_RANGE:
             ax.yaxis.set_minor_locator(ticker.AutoMinorLocator(2))
+        if coord is Coords.GROUND_SCATTER_MAPPED_RANGE:
+            ax.yaxis.set_minor_locator(ticker.AutoMinorLocator(2))
         else:
             ax.yaxis.set_minor_locator(ticker.MultipleLocator(5))
         # so the plots gets to the ends
