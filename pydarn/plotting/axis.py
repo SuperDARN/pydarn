5f8724d640691821cb1492e1662298fac7de9fe8
mts299 <marina.t.schmidt@gmail.com>
2021-04-29 13:15:02 -0600

    PEP 8 stylee

--- pydarn/plotting/axis.py
+++ pydarn/plotting/axis.py
@@ -18,21 +18,22 @@ Code which generates axis objects for use in plotting functions
 import matplotlib.pyplot as plt
 import numpy as np
 
+
 class Projections():
     """
         Methods for generating axis objects for use in plotting functions
         Methods
         -------
         axis_polar
-        """
+    """
 
     def __str__(self):
         return "This class is static class that provides"\
                 " the following methods: \n"\
-                "   - axis_polar()\n"\
+                "   - axis_polar()\n"
 
     @classmethod
-    def axis_polar(cls, lowlat: int = 30, hem: str='N'):
+    def axis_polar(cls, lowlat: int = 30, hem: str = 'N'):
         """
         Plots a radar's Field Of View (FOV) fan plot for the given data and
         scan number
@@ -45,9 +46,9 @@ class Projections():
             hem: str
                 Hemisphere of polar projection. Can be 'N' or 'S' for
                 northern and southern hemispheres, respectively
-                Default: 'N'         
-        """                    
-        
+                Default: 'N'
+        """
+
         ax = plt.axes(polar=True)
 
         # Set upper and lower latitude limits (pole and lowlat)
@@ -55,16 +56,17 @@ class Projections():
             ax.set_ylim(90, lowlat)
             ax.set_yticks(np.arange(lowlat, 90, 10))
         else:
-            ax.set_ylim(-90, -abs(lowlat)) # If hem is 'S', lowlat must be negative
+            # If hem is 'S', lowlat must be negative
+            ax.set_ylim(-90, -abs(lowlat))
             ax.set_yticks(np.arange(-abs(lowlat), -90, -10))
-         
-        # Locations of tick marks. Will be customisable in future    
+
+        # Locations of tick marks. Will be customisable in future
         ax.set_xticks([0, np.radians(45), np.radians(90), np.radians(135),
                        np.radians(180), np.radians(225), np.radians(270),
                        np.radians(315)])
-                       
-        # Tick labels will depend on coordinate system               
+
+        # Tick labels will depend on coordinate system
         ax.set_xticklabels(['00', '', '06', '', '12', '', '18', ''])
         ax.set_theta_zero_location("S")
-        
-        return ax 
+
+        return ax
