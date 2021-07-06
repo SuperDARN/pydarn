5b449dfedfb820b302ccf5039ba78b3a31120dea
carleyjmartin <carleymartin@hotmail.com>
2021-07-06 13:31:54 -0600

    added modification line - checked copyright

--- pydarn/plotting/rtp.py
+++ pydarn/plotting/rtp.py
@@ -6,6 +6,7 @@
 # Modifications:
 # 2021-05-12 Francis Tholley added gate2grounscatter to range-time plots
 # 2021-06-18 Marina Schmidt (SuperDARN Canada) fixed ground scatter colour bug
+# 2021-07-06 Carley Martin added keyword to aid in rounding start times
 # Disclaimer:
 # pyDARN is under the LGPL v3 license found in the root directory LICENSE.md
 # Everyone is permitted to copy and distribute verbatim copies of this license
