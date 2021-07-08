cb84c732f245f5eeff9cfa2e2318b77ab6747c28
mts299 <marina.t.schmidt@gmail.com>
2021-07-08 13:59:26 -0600

    added disclaimer

--- test/test_RTP.py
+++ test/test_RTP.py
@@ -1,3 +1,17 @@
+# Copyright (C) 2020 SuperDARN Canada, University of Saskatchewan
+# Author: Marina Schmidt
+#
+# Modifications:
+#
+# Disclaimer:
+# pyDARN is under the LGPL v3 license found in the root directory LICENSE.md
+# Everyone is permitted to copy and distribute verbatim copies of this license
+# document, but changing it is not allowed.
+#
+# This version of the GNU Lesser General Public License incorporates the terms
+# and conditions of version 3 of the GNU General Public License,
+# supplemented by the additional permissions listed below.
+
 import bz2
 import pytest
 
