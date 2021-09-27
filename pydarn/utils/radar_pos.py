9ca798b102063fca41b508f83e385c4da5888918
mts299 <marina.t.schmidt@gmail.com>
2021-09-27 10:58:22 -0600

    add modification line for Francis's changes for virtual height

--- pydarn/utils/radar_pos.py
+++ pydarn/utils/radar_pos.py
@@ -18,7 +18,7 @@
 # Modifications:
 #   2020-04-20 Marina Schmidt converted the above link to python and changed
 #              variable and function names to readability
-#
+#   2021-09-15 Francis Tholley relocated the virtual height models to another file
 # Disclaimer:
 # pyDARN is under the LGPL v3 license found in the root directory LICENSE.md
 # Everyone is permitted to copy and distribute verbatim copies of this license
@@ -276,10 +276,10 @@ def geocentric_coordinates(radar_lat: float, radar_lon: float,
     """
     if virtual_height_type == VH_types.CHISHAM:
         x_height = chisham(slant_range)
-                 
+
     if virtual_height_type == VH_types.STANDARD_VIRTUAL_HEIGHT:
         x_height = standard_virtual_height(slant_range, cell_height)
-        
+
     # calculate the radius over the earth underneath
     # the radar and range gate cell
     rlat, rlon, r_radar, delta = geodetic2geocentric(radar_lat, radar_lon)
