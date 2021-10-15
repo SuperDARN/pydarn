16c785e0e7093a850098aa86728d7638f882546e
Francis Tholley <francis.tholley@scranton.edu>
2021-10-14 22:01:33 -0400

    Working

--- pydarn/plotting/fan.py
+++ pydarn/plotting/fan.py
@@ -361,6 +361,7 @@ class Fan():
                     r = [rs[gates, beams], rs[gates + 1, beams],
                          rs[gates + 1, beams + 1], rs[gates, beams + 1]]
             if boundary:
+                import pdb; pdb.set_trace()
                 """lons = np.concatenate(
                      (thetas[ranges[0], :],
                       thetas[ranges[0]:ranges[1], -1],
