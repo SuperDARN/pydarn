9dba19530625b766fc87e03665a6d7218a67ac4e
shibaji7 <shibaji7@vt.edu>
2021-10-28 11:10:52 -0400

    Initialize boxcar_filter.py

---
+++ pydarn/utils/boxcar_filter.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+"""boxcar_filter.py: Module is dedicated to run 3X3X3 boxcar filtering."""
+
+__author__ = "Chakraborty, S."
+__credits__ = []
+__version__ = "1.0."
+__maintainer__ = "Chakraborty, S."
+__email__ = "shibaji7@vt.edu"
+__status__ = "Research"
+__copyright__ = ""
+__license__ = ""
+
