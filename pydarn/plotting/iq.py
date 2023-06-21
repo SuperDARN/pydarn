2b8d250b928727265c39c6b2b14eb287b8d542aa
carleyjmartin <carleymartin@hotmail.com>
2023-06-21 15:57:43 -0600

    add phase option plot

--- pydarn/plotting/iq.py
+++ pydarn/plotting/iq.py
@@ -18,51 +18,50 @@
 """
 IQ plots
 """
-import copy
 import matplotlib.pyplot as plt
 import numpy as np
 import warnings
 
-from datetime import datetime, timedelta
-from matplotlib import dates, colors, cm, ticker
+from datetime import datetime
 from typing import List
 
 from pydarn import (time2datetime, standard_warning_format, find_record)
 
 warnings.formatwarning = standard_warning_format
 
+
 class IQ():
     """
     Time-series and range-time plots for IQ data
-    
+
     Class pattern design: Builder
     This class inherits matplotlib.pyplot to inherit plotting features as well
     build off their builder design pattern.
-    
+
     Notes
     -----
     This class inherits from matplotlib to generate the figures
-    
+
     Methods
     -------
     plot_iq_time_series
     plot_iq
     """
-    
+
     def __str__(self):
         return "This class is static class that provides"\
                 " the following methods: \n"\
                 "   - plot_time_series()\n"\
                 "   - plot_iq()\n"
 
-
     @classmethod
     def plot_iq(cls, dmap_data: List[dict], start_time: datetime = None,
-                channel: int=0, ax=None, beam_num: int=0,
-                sequence_num: int=0, interferometer: bool=False):
+                channel: int = 0, ax=None, beam_num: int = 0,
+                sequence_num: int = 0, interferometer: bool = False,
+                plot_phase: bool = False):
         """
         Plots IQ data
-        
+
         Parameters
         -----------
         dmap_data: List[dict]
@@ -84,11 +83,9 @@ class IQ():
             Data from main array (False) or interferometer array (True)
             Default False
         """
-        if not ax:
-            ax = plt.gca()
         cls.dmap_data = dmap_data
 
-        # Finds start of minute scan records, then scans the records to 
+        # Finds start of minute scan records, then scans the records to
         # find the next record with the correct beam
         if start_time is not None:
             record_num = find_record(cls.dmap_data, start_time)
@@ -99,8 +96,10 @@ class IQ():
             record_num += 1
             if record_num >= len(dmap_data):
                 raise Exception("No matching data found for beam {} "
-                                "near time {}".format(beam_num,
-                                start_time.strftime('%Y-%m-%d %H:%M:%S')))
+                                "near time {}"
+                                .format(beam_num,
+                                        start_time.strftime('%Y-%m-%d' +
+                                                            ' %H:%M:%S')))
         dmap_record = dmap_data[record_num]
 
         # Details from chosen record
@@ -111,12 +110,12 @@ class IQ():
 
         # Warning to too hihc sequence numberes
         if sequence_num >= seqnum:
-            raise ValueError("Sequence numbers available for this record range "
-                         "from 0 to {}.".format(seqnum-1))
+            raise ValueError("Sequence numbers available for this record "
+                             "range from 0 to {}.".format(seqnum-1))
 
         if interferometer and chnnum < 2:
             raise ValueError("No interferometer data for record chosen.")
-        
+
         # Calculate starting data position
         # For layout of data and how to access see:
         # https://radar-software-toolkit-rst.readthedocs.io
@@ -130,14 +129,27 @@ class IQ():
         smp_initial = smp
         while smp < smp_initial + 2*smpnum:
             iq_real.append(dmap_record['data'][smp])
-            smp=smp+1
+            smp = smp + 1
             iq_imag.append(dmap_record['data'][smp])
-            smp=smp+1
+            smp = smp + 1
 
         # Calculate magnitude and phase
-        mag = [np.sqrt(i**2 + j**2) for i,j in zip(iq_real, iq_imag)]
-        mag_neg = [-np.sqrt(i**2 + j**2) for i,j in zip(iq_real, iq_imag)]
-        phase = np.arctan2(iq_imag,iq_real)
+        mag = [np.sqrt(i**2 + j**2) for i, j in zip(iq_real, iq_imag)]
+        mag_neg = [-np.sqrt(i**2 + j**2) for i, j in zip(iq_real, iq_imag)]
+
+        # Plotting phase if chosen
+        if plot_phase:
+            phase = np.arctan2(iq_imag, iq_real)
+            fig = plt.gcf()
+            gs = fig.add_gridspec(3, 1)
+            ax = fig.add_subplot(gs[0:2, :])
+            ax2 = fig.add_subplot(gs[2, :])
+            phase_line, = ax2.plot(phase, 'mediumseagreen')
+            ax2.grid()
+            ax2.set_ylabel('Phase')
+            ax2.set_xlabel('Sample Number')
+        else:
+            ax = plt.gca()
 
         # Plot lines
         real_line, = ax.plot(iq_real, 'r')
@@ -152,7 +164,7 @@ class IQ():
         ax.legend()
 
         # Plot title and axis labels
-        #plt.plot(phase, 'y')
+        # plt.plot(phase, 'y')
         if interferometer:
             array_type = 'Interferometer Array'
         else:
