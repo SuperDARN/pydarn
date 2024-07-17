4f87a3a95c38e664309932e1ed404e9237379e3d
carleyjmartin <carleymartin@hotmail.com>
2024-07-17 11:49:55 -0600

    missed dt from merge conflicts

--- pydarn/utils/plotting.py
+++ pydarn/utils/plotting.py
@@ -162,8 +162,8 @@ def time2datetime(dmap_record: dict) -> dt.datetime:
         hour = dmap_record['start.hour']
         minute = dmap_record['start.minute']
         second = dmap_record['start.second']
-        return datetime(year=year, month=month, day=day, hour=hour,
-                        minute=minute, second=int(second))
+        return dt.datetime(year=year, month=month, day=day, hour=hour,
+                           minute=minute, second=int(second))
 
 
 def add_embargo(fig: plt.Figure):
